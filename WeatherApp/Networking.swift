f3c29ae07f6f29e9b00167dd54c8592fb456f650
Navruzova <haughtygrumbler@gmail.com>
2022-08-16 20:46:46 +0300

    Added LocationManager

---
+++ WeatherApp/Networking.swift
@@ -0,0 +1,18 @@
+//
+//  Networking.swift
+//  WeatherApp
+//
+//  Created by Александра Наврузова on 22.06.2022.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherAPI {
+  
+  let weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={e9f8acf7be36bcab6b29389405653cc2}"
+
+  func fetchCurrentWeather() {
+    
+  }
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+  let locationManager = CLLocationManager()
+  var location: CLLocation?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    checkLocationServices()
   }
-
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+  
+  func setupLocationManager() {
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+  }
+  
+  func checkLocationServices() {
+    if CLLocationManager.locationServicesEnabled() {
+      setupLocationManager()
+    } else {
+      
+    }
+  }
+  
+  func getUserCoordinates() {
+    if let coordinates = locationManager.location?.coordinate {
+      print("Latitude: \(coordinates.latitude), Longitude: \(coordinates.longitude)")
+    }
+    DispatchQueue.main.async {
+      
+    }
+  }
+  
+  func checkLocationAuthorization() {
+    switch locationManager.authorizationStatus {
+    case .authorizedWhenInUse:
+      getUserCoordinates()
+      locationManager.startUpdatingLocation()
+      break
+    case .denied:
+      break
+    case .notDetermined:
+      locationManager.requestWhenInUseAuthorization()
+      break
+    case .restricted:
+      break
+    default:
+      break
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    checkLocationAuthorization()
+  }
+  
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    checkLocationAuthorization()
+  }
+}
